3372a6bf500cd0065e399948ccd202d902945f30
johnomernik <john@omernik.com>
2019-04-08 14:26:50 -0500

    Initial Files

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#sudo pip3 install RPI.GPIO
+#sudo pip3 install adafruit-blinka
+
+
+sudo pip3 install adafruit-circuitpython-ssd1306
+sudo pip3 install adafruit-circuitpython-framebuf
+sudo pip3 install adafruit-circuitpython-rfm9x
